4d3b189e87e1cc8d0fccab79420319cbc4e005dc
cactus-actuarial <48238628+cactus-actuarial@users.noreply.github.com>
2019-03-05 18:35:19 +0100

    All the files

---
+++ footer.php
@@ -0,0 +1,17 @@
+		</div>
+	</div>
+
+	
+	<div style="clear: both;"></div>
+	<footer>	
+		<div style="float: left; width: 80%;">
+			<div style="position: relative; left: 96px;">
+				<p><?php echo date("Y"); ?></p>
+			</div>
+		</div>
+	</footer>
+	
+	</body>
+	
+	
+</html>
